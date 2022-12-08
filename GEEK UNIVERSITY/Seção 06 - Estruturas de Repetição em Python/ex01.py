63af546b5142f09f92c419463764e962ca2ad30c
Rubem Álecks <rubemalecks@gmail.com>
2022-12-07 23:22:06 -0300

    up desc

--- GEEK UNIVERSITY/Seção 06 - Estruturas de Repetição em Python/ex01.py
+++ GEEK UNIVERSITY/Seção 06 - Estruturas de Repetição em Python/ex01.py
@@ -1,6 +1,5 @@
 print('''
-Faça um programa que determine e mostre os cinco primeiros multiplos de 3, 
-considerando números maiores que 0.
+Faça um programa que determine e mostre os cinco primeiros múltiplos de 3, considerando números maiores que 0.
 ''')
 
 for x in range(3,18,3):
